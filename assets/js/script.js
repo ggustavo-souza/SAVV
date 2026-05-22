43ecb95f7d5c5c6f8188c0d8639c43c45559d789
ggustavo-souza <cavalogames1231@gmail.com>
2026-05-22 19:47:55 -0300

    feat: transição no botão e ocultação da logo quando o menu hamburguer for ativado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
 const menu = document.querySelector('.btnMenuHamburguer');
+const logo = document.querySelector('.logoNav');
 
 menu.addEventListener('click', () => {
     const navLi = document.querySelectorAll('.navbar ul li');
     navLi.forEach((li) => {
         li.classList.toggle('active');
     });
+    logo.classList.toggle('active');
 });
