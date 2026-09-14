f4490e5b2d4f4f408d47e82c19e9e30be23de635
ggustavo-souza <cavalogames1231@gmail.com>
2026-09-14 10:20:27 -0300

    fix: ajustando responsividade do navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,33 +1,33 @@
 export default function Navbar() {
   return (
-    <header className="w-full shadow-sm border-b border-gray-200 bg-primaria">
+    <header className="w-full shadow-sm border-b h-fit border-gray-200 bg-primaria">
       <nav className="flex w-full justify-between px-10">
         <div className="">
           <a href="/" className="">
-            <img src="/SAVV_Logo_SemFundo.png" alt="SAVV Logo" className="h-full w-auto" />
+            <img src="/SAVV_Logo_SemFundo.png" alt="SAVV Logo" className="h-18 sm:h-22 md:h-24 xl:h-26 w-auto" />
           </a>
         </div>
 
         <ul className="flex items-center gap-6">
           <li>
-            <a href="/" className="font-medium text-xl text-secundaria transition-colors hover:opacity-80">
+            <a href="/" className="font-medium lg:text-lg text-secundaria transition-colors hover:opacity-80">
               Home
             </a>
           </li>
           <li>
-            <a href="/servicos" className="font-medium text-xl text-secundaria transition-colors hover:opacity-80">
+            <a href="/servicos" className="font-medium lg:text-lg text-secundaria transition-colors hover:opacity-80">
               Serviços
             </a>
           </li>
           <li>
-            <a href="/denuncie" className="font-medium text-xl text-secundaria transition-colors hover:opacity-80">
+            <a href="/denuncie" className="font-medium lg:text-lg text-secundaria transition-colors hover:opacity-80">
               Denuncie
             </a>
           </li>
           <li>
             <a
               href="/login"
-              className="font-medium px-6 py-2 text-xl rounded-sm shadow-sm bg-secundaria text-primaria transition-colors hover:opacity-90"
+              className="font-medium px-6 py-2 lg:text-lg rounded-sm shadow-sm bg-secundaria text-primaria transition-colors hover:opacity-90"
             >
               Login
             </a>
