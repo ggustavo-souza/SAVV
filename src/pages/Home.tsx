18366d7a8c4242e01d33e4878d2828b102962c3b
ggustavo-souza <cavalogames1231@gmail.com>
2026-09-15 15:30:04 -0300

    fix: ajustando alinhamento e estilização dos dados de transparência

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,20 +10,20 @@ export default function Home() {
                 <p className="text-xl text-center font-normal text-primaria z-10 select-none max-w-2xl">Uma plataforma para a solicitação de serviços ambientais. Transparência total para o cidadão.</p>
                 <button className="bg-primaria z-10 mt-4 cursor-pointer text-secundaria font-bold py-3 px-10 rounded-xs hover:scale-105 hover:brightness-90 transition-transform duration-200">Solicitar Serviço</button>
             </div>
-            <section className="w-full h-fit bg-primaria flex flex-row items-center justify-center gap-6 py-20">
-                <div className="w-full h-fit flex flex-col items-start justify-center gap-2 px-10">
-                    <p className="text-xl z-10 font-bold text-center text-gray-500">Total de Serviços Solicitados</p>
-                    <h1 className="text-7xl mt-2 font-extrabold text-center text-secundaria">87</h1>
+            <section className="w-full h-fit bg-primaria flex flex-row justify-between gap-6 py-20 mx-auto px-10">
+                <div className="h-fit flex flex-col items-start justify-center gap-2 w-fit">
+                    <p className="text-xl z-10 font-bold text-center text-gray-500">Serviços Solicitados</p>
+                    <h1 className="text-8xl mt-2 font-extrabold text-center text-secundaria">87</h1>
                 </div>
 
-                <div className="w-full h-fit flex flex-col items-start justify-center gap-2 px-10">
-                    <p className="text-xl z-10 font-bold text-center text-gray-500">Total de Serviços Solicitados</p>
-                    <h1 className="text-7xl mt-2 font-extrabold text-center text-secundaria">87</h1>
+                <div className=" h-fit flex flex-col items-start justify-center gap-2 w-fit">
+                    <p className="text-xl z-10 font-bold text-center text-gray-500">Serviços Concluídos</p>
+                    <h1 className="text-8xl mt-2 font-extrabold text-center text-secundaria">72</h1>
                 </div>
 
-                <div className="w-full h-fit flex flex-col items-start justify-center gap-2 px-10">
-                    <p className="text-xl z-10 font-bold text-center text-gray-500">Total de Serviços Solicitados</p>
-                    <h1 className="text-7xl mt-2 font-extrabold text-center text-secundaria">87</h1>
+                <div className=" h-fit flex flex-col items-start justify-center gap-2 w-fit">
+                    <p className="text-xl z-10 font-bold text-center text-gray-500">Árvores Plantadas</p>
+                    <h1 className="text-8xl mt-2 font-extrabold text-center text-secundaria">150</h1>
                 </div>
             </section>
         </>
