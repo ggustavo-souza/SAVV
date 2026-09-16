b5772cf50e6f558540cfdf86bd1109128853c7e0
ggustavo-souza <cavalogames1231@gmail.com>
2026-09-16 16:49:56 -0300

    feat: adicionando a navbar em todas as páginas criadas até então

--- src/pages/Servicos.tsx
+++ src/pages/Servicos.tsx
@@ -1,8 +1,13 @@
+import Navbar from "../components/Navbar";
+
 export default function Servicos() {
     return (
+        <>
+        <Navbar />
         <div className="w-full h-screen flex flex-col items-center justify-center gap-6">
             <h1 className="text-3xl font-bold text-secundaria">Serviços</h1>
             <p className="text-lg text-secundaria">Esta página ainda não foi implementada.</p>
         </div>
+        </>
     )
 }
