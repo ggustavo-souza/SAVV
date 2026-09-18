4c89130d9170a372e902ba302333638dc22c8052
ggustavo-souza <cavalogames1231@gmail.com>
2026-09-18 10:12:13 -0300

    feat: dando início na responsividade da navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,7 @@
 export default function Navbar() {
+
+
+
   return (
     <header className="w-full shadow-sm border-b h-fit border-gray-200 bg-primaria">
       <nav className="flex w-full justify-between px-10">
@@ -10,24 +13,24 @@ export default function Navbar() {
 
         <ul className="flex items-center gap-6">
           <li>
-            <a href="/" className="font-medium lg:text-md xl:text-lg  text-secundaria transition-colors hover:opacity-80">
+            <a href="/" className="hidden md:block font-medium lg:text-md xl:text-lg  text-secundaria transition-colors hover:opacity-80">
               Home
             </a>
           </li>
           <li>
-            <a href="/transparencia" className="font-medium lg:text-md xl:text-lg text-secundaria transition-colors hover:opacity-80">
+            <a href="/transparencia" className="hidden md:block font-medium lg:text-md xl:text-lg text-secundaria transition-colors hover:opacity-80">
               Transparência
             </a>
           </li>
           <li>
-            <a href="/servico" className="font-medium lg:text-md xl:text-lg text-secundaria transition-colors hover:opacity-80">
+            <a href="/servico" className="hidden md:block font-medium lg:text-md xl:text-lg text-secundaria transition-colors hover:opacity-80">
               Serviço
             </a>
           </li>
           <li>
             <a
               href="/login"
-              className="font-medium px-6 py-2 lg:text-md xl:text-lg rounded-xs shadow-sm bg-secundaria text-primaria transition-colors hover:opacity-90"
+              className="hidden md:block font-medium px-6 py-2 lg:text-md xl:text-lg rounded-xs shadow-sm bg-secundaria text-primaria transition-colors hover:opacity-90"
             >
               Login
             </a>
